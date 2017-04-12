f40a8dbe47e61a7de048e670eb7ad4b132c279b4
Tomasz Masternak <tomasz.masternak@gmail.com>
2017-04-12 14:49:30 +0200

    ado transactions and transaction scope

---
+++ Scenarios/NativeTransactionAndTransactionScope.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Transactions;
+using NUnit.Framework;
+
+namespace Scenarios
+{
+    [TestFixture]
+    public class NativeTransactionAndTransactionScope
+    {
+        const string ConnectionString = @"Server=DESKTOP-G5E7IEI;Database=master;Trusted_Connection=True;";
+        const string ConnectionStringReformated = @"Database=master;Server=DESKTOP-G5E7IEI;Trusted_Connection=True;";
+
+        [Test]
+        public async Task Ado_transactions_completes_independently_from_scope()
+        {
+            using (new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                using (var connection = new SqlConnection(ConnectionString))
+                {
+                    await connection.OpenAsync();
+
+                    using (var adoTransaction = connection.BeginTransaction())
+                    {
+                        var nativeCommand = new SqlCommand("create table test(id int)", connection);
+
+                        nativeCommand.ExecuteNonQuery();
+
+                        adoTransaction.Rollback();
+                    }
+                }
+
+                using (var connection = new SqlConnection(ConnectionStringReformated))
+                {
+                    Assert.ThrowsAsync<TransactionAbortedException>(
+                        async () => await connection.OpenAsync(),
+                        "TransactionScope tries to Promote adoTransaction but it's already disposed");
+                }
+            }
+        }
+
+    }
+}
